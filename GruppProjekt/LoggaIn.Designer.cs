78157fce3a458d587bdf99ea1b19cdcc00077762
JennyAndersen <jenny.andersen@student.nbi-handelsakademin.se>
2023-02-28 08:59:09 +0100

    Ändrade lite storlek på bilderna

--- GruppProjekt/LoggaIn.Designer.cs
+++ GruppProjekt/LoggaIn.Designer.cs
@@ -46,10 +46,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(8, 138);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 212);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(186, 168);
+            this.pictureBox1.Size = new System.Drawing.Size(279, 258);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -57,10 +56,9 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(473, 8);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Location = new System.Drawing.Point(710, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(52, 42);
+            this.pictureBox2.Size = new System.Drawing.Size(78, 65);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
@@ -68,29 +66,26 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(223, 99);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(334, 152);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(175, 29);
+            this.textBox1.Size = new System.Drawing.Size(260, 39);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(223, 141);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Location = new System.Drawing.Point(334, 217);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(175, 29);
+            this.textBox2.Size = new System.Drawing.Size(260, 39);
             this.textBox2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(179, 45);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(268, 69);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(175, 37);
+            this.label1.Size = new System.Drawing.Size(256, 55);
             this.label1.TabIndex = 2;
             this.label1.Text = "Inloggning";
             // 
@@ -98,10 +93,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(116, 144);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(174, 222);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 24);
+            this.label2.Size = new System.Drawing.Size(141, 32);
             this.label2.TabIndex = 2;
             this.label2.Text = "Lösenord";
             // 
@@ -109,10 +103,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(63, 102);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(94, 157);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(151, 24);
+            this.label3.Size = new System.Drawing.Size(219, 32);
             this.label3.TabIndex = 2;
             this.label3.Text = "Användarnamn";
             // 
@@ -120,10 +113,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(370, 251);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(526, 387);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(132, 24);
+            this.label4.Size = new System.Drawing.Size(195, 32);
             this.label4.TabIndex = 3;
             this.label4.Text = "Administratör";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -131,9 +123,10 @@
             // btnLogin
             // 
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(252, 187);
+            this.btnLogin.Location = new System.Drawing.Point(378, 288);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(102, 32);
+            this.btnLogin.Size = new System.Drawing.Size(153, 49);
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "Logga in";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -142,9 +135,10 @@
             // 
             this.LinkRegistrering.AutoSize = true;
             this.LinkRegistrering.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LinkRegistrering.Location = new System.Drawing.Point(248, 233);
+            this.LinkRegistrering.Location = new System.Drawing.Point(328, 386);
+            this.LinkRegistrering.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LinkRegistrering.Name = "LinkRegistrering";
-            this.LinkRegistrering.Size = new System.Drawing.Size(111, 24);
+            this.LinkRegistrering.Size = new System.Drawing.Size(173, 33);
             this.LinkRegistrering.TabIndex = 5;
             this.LinkRegistrering.TabStop = true;
             this.LinkRegistrering.Text = "Registrering";
@@ -152,11 +146,11 @@
             // 
             // LoggaIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(542, 310);
+            this.ClientSize = new System.Drawing.Size(813, 477);
             this.Controls.Add(this.LinkRegistrering);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.label4);
@@ -168,7 +162,6 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LoggaIn";
             this.Text = "LoggaIn";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
