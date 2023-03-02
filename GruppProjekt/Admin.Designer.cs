73fddad80ed7928df77336b3d673ad9a7e72bcb2
JennyAndersen <jenny.andersen@student.nbi-handelsakademin.se>
2023-03-02 14:09:08 +0100

    Knappfunktionerna

--- GruppProjekt/Admin.Designer.cs
+++ GruppProjekt/Admin.Designer.cs
@@ -30,11 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnVisa = new System.Windows.Forms.Button();
+            this.txtbSok = new System.Windows.Forms.TextBox();
+            this.btnSok = new System.Windows.Forms.Button();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnNollstall = new System.Windows.Forms.Button();
+            this.gridPersonal = new System.Windows.Forms.DataGridView();
             this.btnRadera = new System.Windows.Forms.Button();
             this.btnAndra = new System.Windows.Forms.Button();
             this.btnSpara = new System.Windows.Forms.Button();
@@ -52,18 +54,20 @@
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridPersonal)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnVisa);
+            this.panel1.Controls.Add(this.txtbSok);
+            this.panel1.Controls.Add(this.btnSok);
             this.panel1.Controls.Add(this.pictureBox3);
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.dataGridView1);
-            this.panel1.Controls.Add(this.btnNollstall);
+            this.panel1.Controls.Add(this.gridPersonal);
             this.panel1.Controls.Add(this.btnRadera);
             this.panel1.Controls.Add(this.btnAndra);
             this.panel1.Controls.Add(this.btnSpara);
@@ -83,12 +87,45 @@
             this.panel1.Size = new System.Drawing.Size(1219, 625);
             this.panel1.TabIndex = 0;
             // 
+            // btnVisa
+            // 
+            this.btnVisa.BackColor = System.Drawing.Color.LightGreen;
+            this.btnVisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVisa.Location = new System.Drawing.Point(15, 220);
+            this.btnVisa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnVisa.Name = "btnVisa";
+            this.btnVisa.Size = new System.Drawing.Size(153, 49);
+            this.btnVisa.TabIndex = 24;
+            this.btnVisa.Text = "Visa Lista";
+            this.btnVisa.UseVisualStyleBackColor = false;
+            this.btnVisa.Click += new System.EventHandler(this.btnVisa_Click);
+            // 
+            // txtbSok
+            // 
+            this.txtbSok.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.txtbSok.Location = new System.Drawing.Point(866, 232);
+            this.txtbSok.Name = "txtbSok";
+            this.txtbSok.Size = new System.Drawing.Size(175, 26);
+            this.txtbSok.TabIndex = 23;
+            // 
+            // btnSok
+            // 
+            this.btnSok.BackColor = System.Drawing.Color.LightGreen;
+            this.btnSok.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSok.Location = new System.Drawing.Point(1048, 220);
+            this.btnSok.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSok.Name = "btnSok";
+            this.btnSok.Size = new System.Drawing.Size(153, 49);
+            this.btnSok.TabIndex = 22;
+            this.btnSok.Text = "Sök Personalnamn";
+            this.btnSok.UseVisualStyleBackColor = false;
+            // 
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(281, 14);
+            this.pictureBox3.Location = new System.Drawing.Point(212, 7);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(48, 36);
+            this.pictureBox3.Size = new System.Drawing.Size(112, 83);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 21;
             this.pictureBox3.TabStop = false;
@@ -96,9 +133,9 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(579, 14);
+            this.pictureBox2.Location = new System.Drawing.Point(605, 7);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(46, 36);
+            this.pictureBox2.Size = new System.Drawing.Size(115, 83);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 20;
             this.pictureBox2.TabStop = false;
@@ -112,57 +149,48 @@
             this.panel2.Size = new System.Drawing.Size(112, 4);
             this.panel2.TabIndex = 19;
             // 
-            // dataGridView1
+            // gridPersonal
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(18, 277);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1183, 329);
-            this.dataGridView1.TabIndex = 18;
-            // 
-            // btnNollstall
-            // 
-            this.btnNollstall.BackColor = System.Drawing.Color.LightGreen;
-            this.btnNollstall.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNollstall.Location = new System.Drawing.Point(758, 220);
-            this.btnNollstall.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnNollstall.Name = "btnNollstall";
-            this.btnNollstall.Size = new System.Drawing.Size(153, 49);
-            this.btnNollstall.TabIndex = 17;
-            this.btnNollstall.Text = "Nollställ";
-            this.btnNollstall.UseVisualStyleBackColor = false;
+            this.gridPersonal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridPersonal.Location = new System.Drawing.Point(18, 277);
+            this.gridPersonal.Name = "gridPersonal";
+            this.gridPersonal.RowHeadersWidth = 62;
+            this.gridPersonal.RowTemplate.Height = 28;
+            this.gridPersonal.Size = new System.Drawing.Size(1183, 329);
+            this.gridPersonal.TabIndex = 18;
+            this.gridPersonal.SelectionChanged += new System.EventHandler(this.gridPersonal_SelectionChanged_1);
             // 
             // btnRadera
             // 
             this.btnRadera.BackColor = System.Drawing.Color.LightGreen;
             this.btnRadera.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRadera.Location = new System.Drawing.Point(579, 220);
+            this.btnRadera.Location = new System.Drawing.Point(491, 220);
             this.btnRadera.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnRadera.Name = "btnRadera";
             this.btnRadera.Size = new System.Drawing.Size(153, 49);
             this.btnRadera.TabIndex = 16;
             this.btnRadera.Text = "Radera";
             this.btnRadera.UseVisualStyleBackColor = false;
+            this.btnRadera.Click += new System.EventHandler(this.btnRadera_Click);
             // 
             // btnAndra
             // 
             this.btnAndra.BackColor = System.Drawing.Color.LightGreen;
             this.btnAndra.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAndra.Location = new System.Drawing.Point(405, 220);
+            this.btnAndra.Location = new System.Drawing.Point(330, 220);
             this.btnAndra.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAndra.Name = "btnAndra";
             this.btnAndra.Size = new System.Drawing.Size(153, 49);
             this.btnAndra.TabIndex = 15;
             this.btnAndra.Text = "Ändra";
             this.btnAndra.UseVisualStyleBackColor = false;
+            this.btnAndra.Click += new System.EventHandler(this.btnAndra_Click);
             // 
             // btnSpara
             // 
             this.btnSpara.BackColor = System.Drawing.Color.LightGreen;
             this.btnSpara.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSpara.Location = new System.Drawing.Point(226, 220);
+            this.btnSpara.Location = new System.Drawing.Point(169, 220);
             this.btnSpara.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSpara.Name = "btnSpara";
             this.btnSpara.Size = new System.Drawing.Size(153, 49);
@@ -247,7 +275,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(620, 21);
+            this.label2.Location = new System.Drawing.Point(726, 28);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(127, 29);
             this.label2.TabIndex = 5;
@@ -288,7 +316,7 @@
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridPersonal)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -308,13 +336,15 @@
         private System.Windows.Forms.Label lblAdress;
         private System.Windows.Forms.Label lblTelefonnummer;
         private System.Windows.Forms.Label lblNamn;
-        private System.Windows.Forms.Button btnNollstall;
         private System.Windows.Forms.Button btnRadera;
         private System.Windows.Forms.Button btnAndra;
         private System.Windows.Forms.Button btnSpara;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView gridPersonal;
+        private System.Windows.Forms.TextBox txtbSok;
+        private System.Windows.Forms.Button btnSok;
+        private System.Windows.Forms.Button btnVisa;
     }
 }
--- GruppProjekt/Personal.cs
+++ GruppProjekt/Personal.cs
@@ -9,15 +9,18 @@ namespace GruppProjekt
     internal class Personal
     {
         public int id { get; set; }
-        public string name { get; set; }
+        public string namn { get; set; }
         public string telefonnummer { get; set; }
         public string adress { get; set; }
         public string losenord { get; set; }
 
-        public Personal(int id, string name, string telefonnummer, string adress, string losenord)
+        //Lista för personal 
+        public static List<Personal> personal = new List<Personal>();
+
+        public Personal(int id, string namn, string telefonnummer, string adress, string losenord)
         {
             this.id = id;
-            this.name = name;
+            this.namn = namn;
             this.telefonnummer = telefonnummer;
             this.adress = adress;
             this.losenord = losenord;
